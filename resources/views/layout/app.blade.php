31ecc8158aac092c82754f1ac128d7b4fca2c90e
Aditya Wanwade <Adiwanwade@gmail.com>
2023-11-19 17:25:09 +0530

    Added changes to the style of pages

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -10,7 +10,7 @@
     <style>
         .parallax {
           /* The image used */
-          background-image: url("resources/views/layout/163919601_287905782707122_3810949136207511188_n.jpg");
+
 
           /* Set a specific height */
           min-height: 500px;
@@ -31,16 +31,19 @@
 
     <section id="biography">
         <h2 style="padding-bottom: 7px;border-bottom:solid">Bio</h2>
-        <div style="font-size: x-large;font-weight:normal"><pre>
-Arijit Singh is a prolific Indian singer and music composer who has won numerous awards and critical acclaim.
-
+        <div style="font-size: x-large;font-weight:normal;display: flex;
+        align-items: center;
+        justify-content: center;padding:20px">
+            {{-- <div class="gallery-image"> --}}
+                <img src="Images/3565.jpg" alt="Image 5" style="width:555px;height:50%;padding:20px">
+            {{-- </div> --}}
+            Arijit Singh is a prolific Indian singer and music composer who has won numerous awards and critical acclaim.
 In the kaleidoscope of India's vibrant music scene, one name shines with an ethereal brilliance, captivating hearts and evoking deep emotions – Arijit Singh.
 Born on April 25, 1987, in the culturally rich town of Jiaganj, Murshidabad, West Bengal, Arijit emerges as a luminary in the realm of playback singing, his
 mellifluous voice weaving a tapestry of melodies that resonate across generations.Arijit's journey from the quaint bylanes of his childhood to the grand stages
-of Bollywood is a saga of passion, talent, and unwavering determination. Raised in a household where music was not just a form of expression but a way of life,
-his destiny seemed preordained. Guided by a father who recognized the musical prodigy within, Arijit's early years were steeped in the classical tunes of the
-tabla and the soul-stirring cadence of his father's own singing.The world first glimpsed Arijit Singh's vocal prowess on the national stage during his
-participation in the reality show "Fame Gurukul" in 2005. Though the trophy eluded him, it was merely a prelude to a musical odyssey that would soon ensnare
+of Bollywood is a saga of passion, talent, and unwavering determination.
+
+Though the trophy eluded him, it was merely a prelude to a musical odyssey that would soon ensnare
 the hearts of millions. Arijit's breakthrough moment arrived with the haunting "Phir Mohabbat" in "Murder 2" (2011), a serenade that announced the arrival of
 a new voice, a voice that would go on to redefine the contours of romantic ballads in Bollywood.However, it was the magnum opus "Tum Hi Ho" from "Aashiqui 2"
 (2013) that thrust Arijit into the limelight, elevating him to the pantheon of playback legends. This soul-stirring anthem of love not only dominated the charts
@@ -58,7 +61,8 @@ As his melodies continue to soundtrack the lives of millions, Arijit Singh stand
 heard but felt, a celestial force that transcends the boundaries of language and culture, echoing the collective heartbeat of a nation enamored by his art. In
 the symphony of Indian playback singing, Arijit Singh remains the undisputed virtuoso, his voice an everlasting serenade that weaves itself into the fabric of
 our memories.
-</pre></div>
+
+</div>
 
     </section>
 
@@ -147,8 +151,7 @@ our memories.
             <div class="content">
             <h3 class="title" > National Award</h3>
             <p style="font-size: larger">
-                Arijit Singh's National Film Award, one of the highest honors in Indian cinema, stands as a crowning achievement in his illustrious career. The accolade was bestowed upon him in 2014 for the hauntingly beautiful rendition of "Tum Hi Ho" from the film "Aashiqui 2." The song, composed by Mithoon, became an anthem of love, resonating with audiences across the nation. Arijit's soul-stirring performance in "Tum Hi Ho" not only propelled the song to unprecedented heights on music charts but also earned him critical acclaim.
-
+                Arijit Singh's National Film Award, one of the highest honors in Indian cinema, stands as a crowning achievement in his illustrious career. The accolade was bestowed upon him in 2014 for the hauntingly beautiful rendition of "Tum Hi Ho" from the film "Aashiqui 2." The song, composed by Mithoon, became an anthem of love, resonating with audiences across the nation.
 The National Film Award for Best Playback Singer (Male) acknowledged Arijit Singh's exceptional vocal prowess and his ability to infuse raw emotion into his renditions. This prestigious honor solidified his position as a leading playback singer in the Indian music industry. The National Film Awards are known for their discerning recognition of artistic excellence, making Arijit's win a testament to the enduring impact of his contribution to the cinematic landscape.
 
 Arijit Singh's National Award reflects not only his individual achievement but also the collective appreciation for a song that became a cultural phenomenon. It stands as a symbol of his artistic integrity and the profound connection he forges with listeners through his music, marking a pivotal moment in his career and adding a significant chapter to the rich tapestry of Indian music history.
@@ -172,11 +175,7 @@ Arijit Singh's National Award reflects not only his individual achievement but a
                 <h3 class="title" > Filmfare Award</h3>
                 <p style="font-size: larger">
                     Arijit Singh's Filmfare Awards, a prestigious accolade in the realm of Indian cinema, serve as a glittering testament to his exceptional contributions to playback singing. The Filmfare Awards, often considered the epitome of recognition in the Bollywood industry, have celebrated Arijit's talent with multiple wins in the Best Playback Singer (Male) category.
-
-His journey to Filmfare glory began with the haunting melody "Tum Hi Ho" from the film "Aashiqui 2" (2013), composed by Mithoon. Arijit Singh's soulful rendition of the song not only resonated with audiences but also earned him his first Filmfare Award for Best Playback Singer (Male). This award marked a significant milestone in his career, solidifying his status as a prominent and celebrated playback singer in the Indian music scene.
-
 The subsequent years witnessed a virtual sweep of accolades at the Filmfare Awards for Arijit Singh, with notable wins for songs such as "Chahun Main Ya Naa" ("Aashiqui 2"), "Raabta" ("Agent Vinod"), "Channa Mereya" ("Ae Dil Hai Mushkil"), and many more. His ability to convey a myriad of emotions through his voice, coupled with his versatility across genres, has made him a perennial favorite among Filmfare voters and audiences alike.
-
 Arijit Singh's collection of Filmfare Awards stands as a glittering reflection of his enduring impact on the Indian music industry, where his soulful renditions continue to tug at the heartstrings of millions. Each Filmfare trophy symbolizes not just individual victories but the collective acknowledgment of a maestro whose melodies have woven themselves into the very fabric of Bollywood's musical legacy.
                 </p>
 
@@ -196,13 +195,12 @@ Arijit Singh's collection of Filmfare Awards stands as a glittering reflection o
                     <img src="Images/Screenshot 2023-11-15 231316.png" alt="">
                     <div class="content">
                     <h3 class="title" >IIFA Award</h3>
-                    <p> The International Indian Film Academy (IIFA) Awards is an annual event that honors excellence in the Hindi film industry. Established in 2000, the IIFA Awards are presented by the International Indian Film Academy to recognize outstanding achievements in Bollywood.
+                    <p style="font-size: larger"> The International Indian Film Academy (IIFA) Awards is an annual event that honors excellence in the Hindi film industry. Established in 2000, the IIFA Awards are presented by the International Indian Film Academy to recognize outstanding achievements in Bollywood.
                         .Arijit Singh, the versatile playback singer, has been a prominent recipient of IIFA Awards for his exceptional contributions to the music industry. While specific details may vary each year, Arijit Singh has consistently been recognized in the Best Playback Singer (Male) category.
 
                         Arijit Singh's wins at the IIFA Awards underscore his impact on the industry and his ability to deliver soul-stirring performances across a range of genres. The IIFA Awards are known for their global reach, as the ceremony is often held in different international locations, attracting a diverse audience and showcasing Bollywood on the international stage.
 
-                        Arijit's victories at the IIFA Awards contribute to his overall recognition and acclaim in the realm of playback singing, solidifying his status as one of the leading and celebrated voices in the Indian music scene. His ability to connect with audiences through his emotive renditions has made him a perennial favorite at award ceremonies like IIFA, where his talent is celebrated on a global platform.
-                        He won the Filmfare Award for Best Male Playback Singer for the song "Tum Hi Ho" in 2014. He also won the (IIFA) award in the category of Best Male Singer for the song "Tum Hi Ho" in 2014.
+                         He also won the (IIFA) award in the category of Best Male Singer for the song "Tum Hi Ho" in 2014.
                         It stands as a symbol of his artistic integrity and the profound connection he forges with listeners through his music, marking a pivotal moment in his career and adding a significant chapter to the rich tapestry of Indian music history.
                      </p>
 
